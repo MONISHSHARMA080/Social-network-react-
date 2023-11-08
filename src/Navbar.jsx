afe861d61d15549a387a8cb4c83335bb73eaf03d
unknown <monishsharma080@gmail.com>
2023-11-08 17:24:33 +0530

    navbar 5.88

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -19,78 +19,75 @@ export default function NavBar() {
     navigate("/login");
   }
 
-  return (<>
-    <nav className="flex items-center justify-between flex-wrap">
-      <span
-        className="navbar-brand  flex items-center "
-        style={{
-          backgroundImage: 'linear-gradient(to right, #ffd700, #ff0000)',
-          WebkitBackgroundClip: 'text',
-          backgroundClip: 'text',
-          color: 'transparent',
-        }}
-      >
-        Network
-      </span>
-
-      <div>
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item flex items-center ">
-            <Link className="nav-link flowing-gradient" to="/"  >
-              <motion.button className="nav-link flowing-gradient"  whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}  >
-                All Posts
-              </motion.button>
-            </Link>  
-          </li>
-          <li className="nav-item flex items-center ">
-            <Link className="nav-link flowing-gradient" to="/following">
-              <motion.button className="nav-link flowing-gradient"  whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}  >
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light flex flex-wrap">
+        <div className="flex flex-wrap">
+          <ul className="navbar-nav mr-auto flex flex-wrap">
+            <li className="nav-item active">
+              <Link className="nav-link flowing-gradient text-white" aria-current="page" to="/">
+                <motion.button
+                  className="nav-link flowing-gradient text-white"
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                >
+                  All Posts
+                </motion.button>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link flowing-gradient text-white" to="/following">
+                <motion.button
+                  className="nav-link flowing-gradient text-white"
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                >
                   Following
-              </motion.button>
-            </Link>
-          </li>
-          <li className="nav-item flex items-center ">
-            <Link className="nav-link flowing-gradient" to="New-post">
-              <motion.button className="nav-link flowing-gradient"  whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}  >
-                New post
-              </motion.button>
-            </Link>
-          </li>
-          <li className="nav-item flex items-center ">
-            <Link className="nav-link flowing-gradient" to="/login">
-              <motion.button className="nav-link flowing-gradient"  whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}  >
-                Log in
-              </motion.button>
-            </Link>
-          </li>
-          <li className="nav-item flex items-center ">
-            <Link className="nav-link flowing-gradient" to="/register">
-              <motion.button className="nav-link flowing-gradient"  whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}  >
-                Register
-              </motion.button>
-            </Link>
-          </li>
-          <li className="nav-item flex items-center ">
-            <motion.button className="nav-link flowing-gradient m-2 " onClick={(e)=>{e.preventDefault;logout()}} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} >
-              Log Out
-            </motion.button>
-          </li>
-          
-          {/* if the user is signed in then only print their name  */}
+                </motion.button>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link flowing-gradient text-white" to="/new-post">
+                <motion.button
+                  className="nav-link flowing-gradient text-white"
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                >
+                  New post
+                </motion.button>
+              </Link>
+            </li>
+          </ul>
+
           {user ? (
-          <li className="nav-item flex items-center ">
-            <motion.button className="nav-link flowing-gradient m-2 "  whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} >
-              {user.username}
-            </motion.button>
-          </li> ): null}
+            <li className="nav-item">
+              <motion.button
+                className="nav-link flowing-gradient m-2 text-white"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                {user.username}
+              </motion.button>
+            </li>
+          ) : null}
 
-
-
-
-        </ul>
+          <motion.button
+            className="nav-link flowing-gradient m-2 text-white"
+            onClick={(e) => {
+              e.preventDefault();
+              logout();
+            }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Log Out
+          </motion.button>
+        </div>
+      </nav>
+      <div id="component">
+        <Outlet />
       </div>
-    </nav>
-    <div id="component" ><Outlet /></div>
-  </>);
+    </>
+  );
 }
 

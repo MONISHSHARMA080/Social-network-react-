79337f0b9df32e070d95172931b06247982fdf38
unknown <monishsharma080@gmail.com>
2023-11-08 16:35:26 +0530

    added flexbox in navbar 5.2

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -22,7 +22,7 @@ export default function NavBar() {
   
   return (
     <>
-      <nav className="container mx-auto flex items-center justify-between">
+      <nav className="navbar navbar-expand-lg navbar-light ">
         <span
           className="navbar-brand"
           style={{

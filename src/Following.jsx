76835479ab44b27f6f2f4ad467e2aefaf7e15f67
Monish Sharma <107314033+MONISHSHARMA080@users.noreply.github.com>
2023-11-10 02:13:51 +0530

    Update Following.jsx

--- src/Following.jsx
+++ src/Following.jsx
@@ -5,86 +5,75 @@ import AuthContext from './context/AuthContext';
 import { useNavigate } from "react-router-dom";
 import { motion, useAnimation } from 'framer-motion'; 
 
-
 export default function Following() {
-
     const [data, setData] = useState(null);
-    const {user , authTokens} = useContext(AuthContext)
+    const [loading, setLoading] = useState(true);
+    const { user, authTokens } = useContext(AuthContext);
     const navigate = useNavigate();
 
-    // const headers = {
-    //   'Content-Type': 'application/json',
-    // };
-    
-
-
-
-
     useEffect(() => {
-      // client side routing as unauth. users don't  have following
-      if (user === null){
-        navigate("/");
-     }
-    else{ 
-    fetch(`https://social-network-monish.onrender.com/api/network/${user.user_id}`, {
-        
-      method: 'GET',
-      headers:{
-        'Content-Type': 'application/json',
-        'Authorization':`Bearer ${authTokens.access}`
-      }
-      
-    })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          return response.json();
-        })
+        if (user === null) {
+            navigate("/");
+        } else {
+            fetch(`https://social-network-monish.onrender.com/api/network/${user.user_id}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${authTokens.access}`
+                }
+            })
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! Status: ${response.status}`);
+                    }
+                    return response.json();
+                })
+                .then((json_response) => {
+                    setData(json_response);
+                    setLoading(false); // Set loading to false after data is fetched
+                    console.log(json_response);
+                })
+                .catch((err) => {
+                    console.error(err.message);
+                    setLoading(false); // Set loading to false on error
+                });
+        }
+    }, [user, authTokens.access, navigate]);
 
-        .then((json_response) => {
-          setData(json_response);
-          console.log(json_response)
-        })
-        .catch((err) => {
-          console.error(err.message);
-        });}
-    }, []);
-
-//animation
-const controls = useAnimation();
-const postAnimation = {
- hidden: { opacity: 0, y: 50 },
- visible: { opacity: 1, y: 0 },
-};
-
-
+    // animation
+    const controls = useAnimation();
+    const postAnimation = {
+        hidden: { opacity: 0, y: 50 },
+        visible: { opacity: 1, y: 0 },
+    };
 
     return (
-      <>
-        <h1 className='text-5xl font-bold m-6 p-4 flex-shrink  text-amber-500' > Following:</h1>
-        <div className='post-container'> 
-        {data ? ( data.map((post) => (
-                <motion.div
-                key={post.id}
-                initial="hidden" // Set initial animation state
-                animate="visible" // Set animation state for when the component mounts
-                variants={postAnimation} // Use the animation properties
-              >
-                          <Post
-                          id={post.id}
-                          text={post.text}
-                          owner={post.owner_id}
-                          owner_name={post.owner_name}
-                          date={post.date}
-                          likes={post.likes}
-                          key={post.id}
-                        />
-                        </motion.div>         
-                          ))): (<h2>jbhdchdcbcbhd</h2>) }
-           
-      </div>
-      </>
+        <>
+            <h1 className='text-5xl font-bold m-6 p-4 flex-shrink  text-amber-500'> Following:</h1>
+            <div className='post-container'>
+                {loading ? (
+                    <h2>Loading...</h2>
+                ) : (
+                    data && data.map((post) => (
+                        <motion.div
+                            key={post.id}
+                            initial="hidden"
+                            animate="visible"
+                            variants={postAnimation}
+                        >
+                            <Post
+                                id={post.id}
+                                text={post.text}
+                                owner={post.owner_id}
+                                owner_name={post.owner_name}
+                                date={post.date}
+                                likes={post.likes}
+                                key={post.id}
+                            />
+                        </motion.div>
+                    ))
+                )}
+            </div>
+        </>
     );
-  }
-  
+}
